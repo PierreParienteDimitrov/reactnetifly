9ce4f702b0638cc0ad8cffc7177db91a250bc965
Pierre Pariente <pierre.parientedimitrov@gmail.com>
2021-01-07 14:13:16 -0800

    added preorder and story to skoog play page

--- src/Pages/SkoogPlay.js
+++ src/Pages/SkoogPlay.js
@@ -2,6 +2,8 @@ import React from 'react';
 import { AllFeatures } from '../Components/SkoogPlay/AllFeatures';
 import { FeaturesSkoogPlay } from '../Components/SkoogPlay/FeaturesSkoogPlay';
 import { HeroSkoogPlay } from '../Components/SkoogPlay/HeroSkoogPlay';
+import { Preorder } from '../Components/Preorder';
+import { Story } from '../Components/Story';
 
 export const SkoogPlay = () => {
 	return (
@@ -9,6 +11,8 @@ export const SkoogPlay = () => {
 			<HeroSkoogPlay />
 			<FeaturesSkoogPlay />
 			<AllFeatures />
+			<Preorder />
+			<Story />
 		</div>
 	);
 };
