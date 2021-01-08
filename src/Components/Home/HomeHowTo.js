797024ba425cb8a6fee3a4e6a08f248e3e9de72e
Pierre Pariente <pierre.parientedimitrov@gmail.com>
2021-01-07 16:17:37 -0800

    added full resonsiveness to HOW SKOOG WORKS

--- src/Components/Home/HomeHowTo.js
+++ src/Components/Home/HomeHowTo.js
@@ -4,18 +4,19 @@ import SkoogWorks from '../../images/skoog-works.png';
 import { HowSkoogWorks } from '../../Utils/Content/HowSkoogWorks';
 import { Table } from '../Table';
 import { isXs } from '../../Utils/MediaQueries';
+import AppStores from '../../images/AppStores.png';
 
 export const HomeHowTo = () => {
 	return (
 		<Container
 			style={
 				isXs.matches
-					? { width: '90%', padding: '8rem 0' }
-					: { width: '70%', padding: '14rem 0' }
+					? { width: '94%', padding: '8rem 0', textAlign: 'center' }
+					: { width: '70%', padding: '12rem 0', textAlign: 'left' }
 			}
 		>
 			<Row style={{ alignItems: 'center' }}>
-				<Col sm={12} lg={6}>
+				<Col sm={12} lg={5}>
 					<div
 						style={{
 							width: '100%',
@@ -24,10 +25,10 @@ export const HomeHowTo = () => {
 							marginBottom: '4rem',
 						}}
 					>
-						<img src={SkoogWorks} alt='' width='70%' />
+						<img src={SkoogWorks} alt='' width='80%' />
 					</div>
 				</Col>
-				<Col sm={12} lg={6}>
+				<Col sm={12} lg={7}>
 					<h5>HOW SKOOG WORKS</h5>
 					<br />
 					<br />
@@ -38,6 +39,18 @@ export const HomeHowTo = () => {
 							</div>
 						);
 					})}
+					<br />
+					<br />
+					<h6>Companion App Compatible with</h6>
+					<img
+						src={AppStores}
+						alt='Google Play and App Store'
+						style={
+							isXs.matches
+								? { width: '80%', marginTop: '2rem' }
+								: { width: '50%', marginTop: '2rem' }
+						}
+					/>
 				</Col>
 			</Row>
 		</Container>
--- src/Components/Table.js
+++ src/Components/Table.js
@@ -1,14 +1,27 @@
 import React from 'react';
+import { isXs } from '../Utils/MediaQueries';
 
 export const Table = ({ number, how }) => {
 	return (
 		<div
-			style={{
-				display: 'flex',
-				flexDirection: 'row',
-				alignItems: 'center',
-				marginBottom: '2rem',
-			}}
+			style={
+				isXs.matches
+					? {
+							display: 'flex',
+							flexDirection: 'column',
+							alignItems: 'center',
+							marginBottom: '1.7rem',
+							width: '100%',
+							textAlign: 'center',
+					  }
+					: {
+							display: 'flex',
+							flexDirection: 'row',
+							alignItems: 'center',
+							marginBottom: '1.7rem',
+							width: '80%',
+					  }
+			}
 		>
 			<h2 style={{ width: '10%' }}>{number}</h2>
 			<h4>{how}</h4>
