1ff4bc7ffcc6ad38637f0947ac8b5512f35aa8d9
Pierre Pariente <pierre.parientedimitrov@gmail.com>
2021-01-08 11:28:51 -0800

    updated copy and feature icons

--- src/Components/SkoogPlay/FeatureLeft.js
+++ src/Components/SkoogPlay/FeatureLeft.js
@@ -2,7 +2,14 @@ import React from 'react';
 
 export const FeatureLeft = ({ src, alt, title, longDescription }) => {
 	return (
-		<div style={{ display: 'flex', alignItems: 'center', marginBottom: '2rem' }}>
+		<div
+			style={{
+				display: 'flex',
+				alignItems: 'center',
+				marginBottom: '2rem',
+				justifyContent: 'flex-start',
+			}}
+		>
 			<img src={src} alt={alt} />
 			<div style={{ textAlign: 'left', marginLeft: '2rem', color: 'white' }}>
 				<h5>{title}</h5>
--- src/Components/SkoogPlay/FeatureRight.js
+++ src/Components/SkoogPlay/FeatureRight.js
@@ -2,7 +2,14 @@ import React from 'react';
 
 export const FeatureRight = ({ src, alt, title, longDescription }) => {
 	return (
-		<div style={{ display: 'flex', alignItems: 'center', marginBottom: '2rem' }}>
+		<div
+			style={{
+				display: 'flex',
+				alignItems: 'center',
+				marginBottom: '2rem',
+				justifyContent: 'flex-end',
+			}}
+		>
 			<div style={{ textAlign: 'right', marginRight: '2rem', color: 'white' }}>
 				<h5>{title}</h5>
 				<p>{longDescription}</p>
