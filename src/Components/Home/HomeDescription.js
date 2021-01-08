d1fa7927651dca3d7b1e9353e971eec712c2cbbf
Pierre Pariente <pierre.parientedimitrov@gmail.com>
2021-01-07 23:14:46 -0800

    corrected home

--- src/Components/Home/HomeDescription.js
+++ src/Components/Home/HomeDescription.js
@@ -8,44 +8,41 @@ export const HomeDescription = () => {
 		<Container
 			style={
 				isXs.matches
-					? { width: '90%', marginTop: '4rem', marginBottom: '4rem' }
+					? {
+							width: '90%',
+							marginTop: '4rem',
+							marginBottom: '4rem',
+							textAlign: 'center',
+					  }
 					: {
 							width: '70%',
-							marginTop: '8rem',
-							marginBottom: '10rem',
+							padding: '8rem 0rem',
+							textAlign: 'center',
 					  }
 			}
 		>
-			<div
-				style={
-					isXs
-						? {
-								textAlign: 'center',
-								marginBottom: '1rem',
-						  }
-						: {
-								marginBottom: '1rem',
-						  }
-				}
-			>
+			<Container style={{ marginBottom: '1rem' }}>
 				<h3>EVER EXPANDING CONTENT SPARKS</h3>
 				<br />
 				<h2>NEW ADVENTURES</h2>
-				<div
-					style={{
-						border: 'none',
-						background: `${primary}`,
-						color: 'white',
-						borderRadius: '4rem',
-						height: '2rem',
-						display: 'flex',
-						justifyContent: 'center',
-						alignItems: 'center',
-					}}
-				>
-					<h3>FUN & ENGAGING CONTENT</h3>
-				</div>
-			</div>
+			</Container>
+
+			<Container
+				style={{
+					border: 'none',
+					background: `${primary}`,
+					color: 'white',
+					borderRadius: '4rem',
+					height: '2rem',
+					display: 'flex',
+					justifyContent: 'center',
+					alignItems: 'center',
+					width: '30%',
+					marginBottom: '2rem',
+				}}
+			>
+				<h3>FUN & ENGAGING CONTENT</h3>
+			</Container>
 
 			<h4 style={isXs ? { textAlign: 'center' } : { marginBottom: '1rem' }}>
 				Meet the Skoog. Describe in a few words the what and the who. What is the
